9d3df0ec0195941eed5888bb6fb4ce0ece6761f0
Dertosh <dertosh7@gmail.com>
2017-09-15 15:57:32 +0300

    arr done

--- arr_algs.py
+++ arr_algs.py
@@ -1,9 +1,17 @@
 mas = [8,9,4]
 def my_min(x):
-    return min(x)
+    tmp = mas[0]
+    for n in mas:
+        if n<tmp:
+            tmp = n
+    return tmp
 
 def my_max(x):
-    return max(x)
+    tmp = mas[0]
+    for n in mas:
+        if n>tmp:
+            tmp = n        
+    return tmp
 
 print "min = ", my_min(mas)
 
